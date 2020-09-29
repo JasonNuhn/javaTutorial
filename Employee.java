fa1e1ae79130a3cd635ab5939a3e0f8f36e8e8b0
Jason Nuhn <jasonnuhnpro@Jasons-MacBook-Pro.local>
2020-09-29 10:49:07 -0400

    pushing up progress

--- Employee.java
+++ Employee.java
@@ -1,72 +1,36 @@
-import java.io.*;
-
 public class Employee {
-    
-    // this instance variable is visible for any child class.
-    public String name;
+    private String name;
+    private String address;
+    private int number;
 
-    // salary variable is visible in Employee class only.
-    // private double salary;
-
-    // The name variable is assigned in the constructor.
-    public Employee (String empName) {
-        name = empName;
+    public Employee(String name, String address, int number) {
+        System.out.println("Constructing an Employee");
+        this.name = name;
+        this.address = address;
+        this.number = number;
     }
 
-    public void setSalary(double empSal) {
-        salary = empSal;
+    public void mailCheck() {
+        System.out.println("Mailing a check to " + this.name + " " + this.address);
     }
 
-    // int age;
-    // String designation;
+    public String toString() {
+        return name + " " + address + " " + number;
+    }
 
-    // This is the constructor of the class Employee
-    // public Employee(String name) {
-    //     this.name = name;
-    // }
+    public String getName() {
+        return name;
+    }
 
-    // Assign the age of the Employee to the variable age.
-    // public void empAge(int empAge) {
-    //     age = empAge;
-    // }
+    public String getAddress() {
+        return address;
+    }
 
-    // // Assign the designation to the variable designation
-    // public void empDesignation(String empDesig) {
-    //     designation = empDesig;
-    // }
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
 
-    // Assign the salary to the variable salary
-    // public void empSalary(double empSalary) {
-    //     salary = empSalary;
-    // }
-
-    // Print the employee details
-    // public void printEmployee() {
-    //     System.out.println("Name:" + name);
-    //     System.out.println("Age:" + age);
-    //     System.out.println("Designation:" + designation);
-    //     System.out.println("Salary:" + salary);
-    // }
-
-    // public void printEmp() {
-    //     System.out.println("name : " + name );
-    //     System.out.println("salary : " + salary);
-    // }
-
-    // public static void main(String args[]) {
-    //     Employee empOne = new Employee("Ransika");
-    //     empOne.setSalary(1000);
-    //     empOne.printEmp();
-    // }
-
-    // salary variable is a private static variable
-    private static double salary;
-
-    // DEPARTMENT is a constant
-    public static final String DEPARTMENT = "Development ";
-
-    public static void main(String args[]) {
-        salary = 1000;
-        System.out.println(DEPARTMENT + "average slary:" + salary);
+    public int getNumber() {
+        return number;
     }
 }
--- TestArray.java
+++ TestArray.java
@@ -20,11 +20,11 @@ public class TestArray {
     //     System.out.println("Max is " + max);
     // }
 
-    public static void main(String[] args) {
-        double[] myList = {1.9, 2.9, 3.4, 3.5};
+    // public static void main(String[] args) {
+    //     double[] myList = {1.9, 2.9, 3.4, 3.5};
 
-        for (double element: myList) {
-            System.out.println(element);
-        }
-    }
+    //     for (double element: myList) {
+    //         System.out.println(element);
+    //     }
+    // }
 }
